cab3b2e9792549f1aaea443720e72f4c7fa3bc2c
chase1111125 <157516104+chase1111125@users.noreply.github.com>
2024-08-01 23:19:02 -0500

    Delete models/hvac_testbed directory
    Removed to re-add updated directory

--- models/hvac_testbed/initModel.m
+++
@@ -1,9 +0,0 @@
-% Run this script if you want to run the model without openplc
-simInput = struct("pump", 1, "valve", 1);
-isPaused = false;
-isLogging = false;
-
-if isLogging
-    outputFile = fopen("matlabOutput.json", "wt");
-    fprintf(outputFile, "[");
-end
